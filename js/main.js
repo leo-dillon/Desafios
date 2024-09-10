d03a56323c7ded221df210c2b8faefb96221c7dd
Leonardo Dillon <jeannotegui@gmail.com>
2024-09-10 07:52:38 -0300

    Agregando soluciones y mejorando la lectura

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let problemas =[
+let problemas = [
     {
         id: 1,
         nombre: 'Two Sum',
@@ -16,9 +16,49 @@ let problemas =[
                 }
 
             ],
+        explicacion: [
+            {
+                title: 'function( nums, target)',
+                descripcion: [
+                    'La función tiene 2 argumentos nums (Array de numeros) y target (Número que representa la suma objetivo).',
+                    'El objetivo de la función es encontrar dos índices en el arreglo nums cuyos valores sumen exactamente target.'
+                ]
+            },
+            {
+                title: 'for( let i = 0; i < nums.length; i++)',
+                descripcion: [
+                    'Recorre el array nums, comenzando desde i = 0.'
+                ]
+            },
+            {
+                title: 'for( let j = i + 1; i < nums.length; j++)',
+                descripcion: [
+                    'Recorre el array nums, comenzando desde j = i + 1.'
+                ]
+            },
+            {
+                title: 'if (nums[ i ] + nums[ j ] == target)',
+                descripcion: [
+                    'Este condicional verfica si nums[i] + nums[j] es igual a target.'
+                ]
+            },
+            {
+                title: 'return[ i, j ]',
+                descripcion: [
+                    'En caso de la condición anterior fuera correcta retornamos las posiciones de los valores i y j.',
+                ]
+            },
+            {
+                title: 'Observación',
+                descripcion: [
+                    'En el enuciado del ejercicio nos mensionan que siempre va a ver una respuesta correcta, por lo tanto no necesitamos que halla un retorno de error.'
+                ]
+            }
+            
+        ],
         imagen: './imagenes/desafios/twoSum.png',
         dificultad: 'Easy',
-        desafio: 'https://leetcode.com/problems/two-sum/'
+        desafio: 'https://leetcode.com/problems/two-sum/description/'
     },
     {
         id: 2,
@@ -44,7 +84,25 @@ let problemas =[
             ],
         imagen: './imagenes/desafios/PalindromeNumber.png',
         dificultad: 'Easy',
-        desafio: 'https://leetcode.com/problems/palindrome-number/description/'
+        desafio: 'https://leetcode.com/problems/palindrome-number/description/',
+        explicacion: [
+            {
+                title: 'var reverse = 0',
+                descripcion: [ 'Esta variable almacenará el número invertido.' ]
+            },
+            {
+                title: 'var copy = x',
+                descripcion: [ 'Realizamos una copia del número original, para no modificarlo.' ]
+            },
+            {
+                title: 'while (copy > 0) { }',
+                descripcion: [ 'Este ciclo se ejecuta mientras copy sea mayor que 0, lo que significa que se procesarán los dígitos de COPY uno por uno.' ]
+            },
+            {
+                title: 'const digit = copy % 10',
+                descripcion: [ 'Este ciclo se ejecuta mientras copy sea mayor que 0, lo que significa que se procesarán los dígitos de COPY uno por uno.' ]
+            },
+        ]
     },
     {
         id: 3,
@@ -140,99 +198,126 @@ listaDesaffios.append(crearDesafios( problemas ))
 
 
 function crearDesafios(desafios){
-    let ul = document.createElement('ul')
+    let ul = crearElemento('ul')
     desafios.forEach(UnDesafio => {
-        let li = document.createElement('li')
-        li.classList.add('card')
-        li.innerHTML = `
-            <picture>
-                <img src="${UnDesafio.imagen}" alt="imagen de UnDesafio ${UnDesafio.nombre}">
-                <div class="dificultad">
-                    <p>${UnDesafio.dificultad}</p>
-                </div>
-            </picture>
-            <div class="info">
-                <h3>${UnDesafio.nombre.toUpperCase()}</h3>
-                <p class="m truncado">
-                    ${UnDesafio.enunciado}
-                </p>
-                <a href="${UnDesafio.desafio}" title="Ir a leetCode"> 
-                    Intentar resolverlo 
-                    <picture>
-                        <img src="./imagenes/iconos/sitio-web.png" alt="avatar de Leonardo Dillon">
-                    </picture>
-                </a>
-                <button> Ver solución </button>
-            </div>
-        `
-        li.querySelector('button').addEventListener('click', (e) => {
-            crearSolucion(UnDesafio)
-        })
+        let li = crearElemento('li', 'card')
+        let img_desafio = crearImagen('',UnDesafio.imagen, `imagen de UnDesafio ${UnDesafio.nombre}`)
+        let div_info = crearElemento('div', 'info')
+        let titulo_desafio = crearEncabezado('h3', '', UnDesafio.nombre.toUpperCase())
+        let enuciado_desafio = crearParrafo('m truncado', UnDesafio.enunciado)
+        let link_desafio = crearLink(UnDesafio.desafio, 'Ir a leetCode', '¿Quieres intentarlo?', '', './imagenes/iconos/sitio-web.png', 'Icono tipo Link' )
+        let button_desafio = crearButton('', 'Ver Solución', () => {crearSolucion(UnDesafio)})
+        let div_dificultad = crearElemento('div', 'dificultad')
+        let dificultad = crearParrafo('', UnDesafio.dificultad) 
+        
+        div_info.append(titulo_desafio, enuciado_desafio, link_desafio, button_desafio)
+        div_dificultad.append(dificultad)
+        li.append(img_desafio, div_info, div_dificultad)
         ul.append(li)
     });
     return ul
 }
 function crearSolucion(UnDesafio){
     let main = document.querySelector('main')
-    let sectionRespuesta = crearEelemento('section', 'desafioExpandido')
-    sectionRespuesta.addEventListener('click', () => {
-        sectionRespuesta.remove()
+    let modal = crearElemento('section', 'desafioExpandido')
+    modal.addEventListener('click', () => removerElemento(modal))
+    let div = crearElemento('div')
+    div.addEventListener('click', (e) => { e.stopPropagation(); })
+    let div_title = crearEncabezado('h2','', `${UnDesafio.id} | ${UnDesafio.nombre} <span>${UnDesafio.dificultad}</span>`)
+    let div_enunciado = crearElemento('div', 'desafioExpandido__enunciado')
+    let div_enunciado__title = crearEncabezado('h3', 'subtitulo', 'Enunciado:')
+    let div_enunciado__parrafo = crearParrafo('ejemplo', UnDesafio.enunciado)
+    div_enunciado.append(div_enunciado__title, div_enunciado__parrafo)
+    div.append(div_title ,div_enunciado)
+    UnDesafio.ejemplos.forEach((ej, index) => {
+        let contenedor = crearElemento('div', 'tituloTextoFondo')
+        let contenedor_titulo = crearEncabezado('h3', 'subTitulo', `Ejemplo: ${index+1}`)
+        let contenedor_parrafoInput = crearParrafo('ejemplo', `Input: <span class="destacar">${ej.input}</span>`) 
+        let contenedor_parrafoOutput = crearParrafo('ejemplo', `Output: <span class="destacar">${ej.output}</span>`) 
+        let contenedor_parrafoSubtitulo = crearParrafo('enunciado', 'Explicación:')
+        let contenedor_parrafoExplicacion = crearParrafo('ejemplo', `${ej.explicacion}`)
+        contenedor.append(contenedor_titulo, contenedor_parrafoInput, contenedor_parrafoOutput, contenedor_parrafoSubtitulo, contenedor_parrafoExplicacion)
+        div.append(contenedor)
     })
-    let sectionRespuesta_div = crearEelemento('div')
-    sectionRespuesta_div.addEventListener('click', (e) => {
-        e.stopPropagation();
-    }) 
-    sectionRespuesta_div.innerHTML = `
-        <h2>${UnDesafio.id} | ${UnDesafio.nombre} <span>${UnDesafio.dificultad}</span></h2>
-        <div class="desafioExpandido__enunciado">
-            <h3 class="subTitulo">Enunciado:</h3>
-            <p class="ejemplo">
-                ${UnDesafio.enunciado}
-            </p>
-        </div>
-    `
-    UnDesafio.ejemplos.forEach( (ej, index) => {
-        sectionRespuesta_div.innerHTML += `
-            <div class="desafioExpandido__ejemplo">
-                <h3 class="subTitulo">Ejemplo ${index}:</h3>
-                <p class="ejemplo">
-                    Input: <span class="destacar">${ej.input}</span>
-                </p>
-                <p class="ejemplo">
-                    Output: <span class="destacar">${ej.output}</span>
-                </p>
-                <p class="enunciado">Explicación: </p>
-                <p class="ejemplo">${ej.explicacion}</p>
-            </div>
-        `
+    let div_solucion = crearElemento('div', 'desafioExpandido__solucion')
+    let div_solucion_title = crearEncabezado('h3', 'subTitulo', 'Solución de JavaScript: ')
+    let div_solucion_imagen = crearImagen('', UnDesafio.imagen, `Solición desafio ${UnDesafio.nombre}`)
+    let div_solucion_subTitulo = crearEncabezado('h3', 'subTitulo', 'Explicación.')
+    div_solucion.append(div_solucion_title, div_solucion_imagen, div_solucion_subTitulo)
+    let contenedor = crearElemento('div', 'tituloTextoFondo')
+    UnDesafio.explicacion?.forEach( (linea, index) => {
+        let contenedor__titulo = crearParrafo('enunciado', `${index}. ${linea.title}`)
+        contenedor.append(contenedor__titulo)
+        linea.descripcion.forEach( texto => {
+            let parrafo = crearParrafo('ejemplo', texto)
+            contenedor.append(parrafo)
+        })
+        div_solucion.append(contenedor)
     })
-    sectionRespuesta_div.innerHTML += `
-        <div class="desafioExpandido__solucion">
-            <h3 class="subTitulo">Solucion de JavaScript:</h3>
-            <picture>
-                <img src="${UnDesafio.imagen}" alt="Solucion desafio ${UnDesafio.nombre}">
-            </picture>
-            <p class="enunciado">Explicación</p>
-            <p class="ejemplo">
-            </p>
-        </div>
-    `
-    let btn_cerrarModal = crearEelemento('button', 'cerrarModal')
-    btn_cerrarModal.innerText = 'X'
-    btn_cerrarModal.addEventListener('click', (e) => {
-        sectionRespuesta.remove()
-    })
-    sectionRespuesta_div.append(btn_cerrarModal)
-    sectionRespuesta.append(sectionRespuesta_div)
-    main.append(sectionRespuesta)
+    let div_btnCerrar = crearButton('cerrarModal', 'X', () => removerElemento(modal))
+    div.append(div_solucion, div_btnCerrar)
+    
+
+
+    modal.append(div)
+    main.append(modal)  
 }
-function crearEelemento(elemento, clase, ID){
+function crearElemento(elemento, clase, ID){
     let elementoCreado = document.createElement(elemento)
     if(clase){
-        elementoCreado.classList.add(clase)
+        elementoCreado.setAttribute('class', clase)
     }
     if(ID){
         elementoCreado.setAttribute('id', ID)
     }
     return elementoCreado
 }
+function crearImagen(clase = '', url, texto){
+    let pic = crearElemento('picture', clase)
+    let img = crearElemento('img')
+    img.src = url
+    img.alt = texto
+    pic.append(img)
+    return pic
+}
+function crearEncabezado(h, clase, texto){
+    let encabezado = crearElemento(h, clase)
+    encabezado.innerHTML = texto
+    return encabezado
+}
+function crearParrafo(clase, texto){
+    let p = crearElemento('p', clase)
+    p.innerHTML = texto
+    return p
+}
+function crearLink(destino, title, texto, iconoClase, iconoUrl, iconoTexto){
+    let a = crearElemento('a')
+    a.setAttribute('href', destino)
+    a.setAttribute('title', title)
+    a.innerText = texto
+    if(iconoUrl){
+        let icon = crearImagen(iconoClase, iconoUrl, iconoTexto)
+        a.append(icon)
+    }
+    /*
+        Al imprimir el elemento a por primera vez no muestra el contenido en consola.
+        console.log(a)
+        pero si imprimimos buscando los elementos img si los contine
+        console.log(a.querySelector('img'))
+    */
+    return a
+}
+function crearButton(clase, texto, funcion, iconoClase, iconoUrl, iconoTexto){
+    let button = crearElemento('button', clase)
+    button.innerText = texto
+    button.addEventListener('click', funcion )
+    if(iconoUrl){
+        let icon = crearImagen(iconoClase, iconoUrl, iconoTexto)
+        button.append(icon)
+    }
+    return button
+}
+function removerElemento(elemento){
+    elemento.remove()
+}
+
